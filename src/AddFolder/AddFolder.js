140e864aece3bf07832aff57a17c127081654327
Chayce Knaub <chknaub@yahoo.com>
2020-05-23 19:13:35 -0400

    error boundary component and form validation

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,61 +1,81 @@
-import React, { Component } from 'react'
-import NotefulForm from '../NotefulForm/NotefulForm'
-import ApiContext from '../ApiContext'
-import config from '../config'
-import './AddFolder.css'
+import React, { Component } from "react";
+import NotefulForm from "../NotefulForm/NotefulForm";
+import ApiContext from "../ApiContext";
+import config from "../config";
+import "./AddFolder.css";
+import ErrorBoundary from "../ErrorBoundary";
 
 export default class AddFolder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: {
+        value: "",
+        touched: false,
+      },
+    };
+  }
+
+  updateName(name) {
+    this.setState({ name: { value: name, touched: true } });
+    //console.log(name);
+  }
+
   static defaultProps = {
     history: {
-      push: () => { }
+      push: () => {},
     },
-  }
+  };
   static contextType = ApiContext;
 
-  handleSubmit = e => {
-    e.preventDefault()
+  handleSubmit = (e) => {
+    e.preventDefault();
     const folder = {
-      name: e.target['folder-name'].value
-    }
+      name: e.target["folder-name"].value,
+    };
     fetch(`${config.API_ENDPOINT}/folders`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'content-type': 'application/json'
+        "content-type": "application/json",
       },
       body: JSON.stringify(folder),
     })
-      .then(res => {
-        if (!res.ok)
-          return res.json().then(e => Promise.reject(e))
-        return res.json()
+      .then((res) => {
+        if (!res.ok) return res.json().then((e) => Promise.reject(e));
+        return res.json();
       })
-      .then(folder => {
-        this.context.addFolder(folder)
-        this.props.history.push(`/folder/${folder.id}`)
+      .then((folder) => {
+        this.context.addFolder(folder);
+        this.props.history.push(`/folder/${folder.id}`);
       })
-      .catch(error => {
-        console.error({ error })
-      })
-  }
+      .catch((error) => {
+        console.error({ error });
+      });
+  };
 
   render() {
     return (
-      <section className='AddFolder'>
+      <section className="AddFolder">
         <h2>Create a folder</h2>
-        <NotefulForm onSubmit={this.handleSubmit}>
-          <div className='field'>
-            <label htmlFor='folder-name-input'>
-              Name
-            </label>
-            <input type='text' id='folder-name-input' name='folder-name' />
-          </div>
-          <div className='buttons'>
-            <button type='submit'>
-              Add folder
-            </button>
-          </div>
-        </NotefulForm>
+        <ErrorBoundary>
+          <NotefulForm onSubmit={this.handleSubmit}>
+            <div className="field">
+              <label htmlFor="folder-name-input">Name</label>
+              <input
+                type="text"
+                id="folder-name-input"
+                name="folder-name"
+                onChange={(e) => this.updateName(e.target.value)}
+              />
+            </div>
+            <div className="buttons">
+              <button type="submit" disabled={!this.state.name.touched}>
+                Add folder
+              </button>
+            </div>
+          </NotefulForm>
+        </ErrorBoundary>
       </section>
-    )
+    );
   }
 }
