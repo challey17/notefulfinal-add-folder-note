15f680f1a841a3b6f8d2318e4b4671e87cf90f26
Chayce Knaub <chknaub@yahoo.com>
2020-07-22 21:12:51 -0400

    connecting with server

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -64,7 +64,7 @@ export default class Note extends React.Component {
 
 Note.propTypes = {
   onDeleteNote: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   modified: PropTypes.string.isRequired,
 };
